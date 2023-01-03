8fa53bbac4cc0b1e563534cd0677e706f1d8ca6e
Shirley Ayala <sayalac16_1@unc.edu.pe>
2023-01-03 15:49:46 -0500

    Agregamamos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { OperacionesComponent } from './operaciones/operaciones.component';
+import { FechaComponent } from './fecha/fecha.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    OperacionesComponent,
+    FechaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
